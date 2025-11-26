dfc8fb18c8eefc3c73065ab8f6fc084747221c0c
quic-rnamburi <rnamburi@qti.qualcomm.com>
2025-11-26 14:40:37 +0530

    Update Common.h
    Signed-off-by: quic-rnamburi <rnamburi@qti.qualcomm.com>

--- Core/Modula/Common/Include/Common.h
+++ Core/Modula/Common/Include/Common.h
@@ -17,6 +17,7 @@
 // Used to define the new config value, relative to the already configured value
 #define OPT_WITHREL 0X00000004
 
+
 /**
  * @struct SysResource
  * @brief Used to store information regarding Resources / Tunables which need to be
